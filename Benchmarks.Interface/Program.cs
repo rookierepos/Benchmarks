c8d9cd660af5f8fe75bb527efb252fa590d18987
Yizhe Wang <holysdking@hotmail.com>
2018-12-21 06:04:13 +0800

    IList vs List & Some fix

---
+++ Benchmarks.Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks.Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<LabIEnumerable>();
+            Console.ReadKey();
+        }
+    }
+}
