59a86809b38ff21cf48190aef9165b13d8350a5f
Yizhe Wang <holysdking@hotmail.com>
2019-04-07 18:47:48 +0800

    update dotnet runtime version

--- Benchmarks.Infrastructure/MultipleRuntimes.cs
+++ Benchmarks.Infrastructure/MultipleRuntimes.cs
@@ -14,19 +14,19 @@ namespace Benchmarks.Infrastructure
                 CsProjCoreToolchain.From(
                     new NetCoreAppSettings(
                         targetFrameworkMoniker: "netcoreapp2.1",
-                        runtimeFrameworkVersion: "2.1.7",
+                        runtimeFrameworkVersion: "2.1.9",
                         name: ".NET Core 2.1"))));
             Add(Job.Default.With(
                 CsProjCoreToolchain.From(
                     new NetCoreAppSettings(
                         targetFrameworkMoniker: "netcoreapp2.2",
-                        runtimeFrameworkVersion: "2.2.1",
+                        runtimeFrameworkVersion: "2.2.3",
                         name: ".NET Core 2.2"))));
             Add(Job.Default.With(
                 CsProjCoreToolchain.From(
                     new NetCoreAppSettings(
                         targetFrameworkMoniker: "netcoreapp3.0",
-                        runtimeFrameworkVersion: "3.0.0-preview-27122-01",
+                        runtimeFrameworkVersion: "3.0.0-preview3-27503-5",
                         name: ".NET Core 3.0 preview"))));
         }
     }
